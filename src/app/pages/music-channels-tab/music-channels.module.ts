4510a5ebacf522c9bacdac64cdedd0950b228700
T. Mollov <tundzhaymollov@gmail.com>
2022-07-22 19:37:48 +0300

    removed unnecessary component

--- src/app/pages/music-channels-tab/music-channels.module.ts
+++ src/app/pages/music-channels-tab/music-channels.module.ts
@@ -1,23 +1,21 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { MusicChannelsPage } from './music-channels.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { IonicModule } from "@ionic/angular";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { MusicChannelsPage } from "./music-channels.page";
 
-import { MusicChannelsPageRoutingModule } from './music-channels-routing.module';
-import { HeaderPageModule } from '../header/header.module';
-import { ChannelsGridViewPageModule } from 'src/app/components/channels-grid-view/channels-grid-view.module';
+import { MusicChannelsPageRoutingModule } from "./music-channels-routing.module";
+import { HeaderPageModule } from "../header/header.module";
+import { ChannelsGridViewPageModule } from "src/app/components/channels-grid-view/channels-grid-view.module";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     MusicChannelsPageRoutingModule,
     HeaderPageModule,
-    ChannelsGridViewPageModule
+    ChannelsGridViewPageModule,
   ],
   declarations: [MusicChannelsPage],
 })
--- src/app/pages/tv-channels-tab/tv-channels.module.ts
+++ src/app/pages/tv-channels-tab/tv-channels.module.ts
@@ -1,20 +1,18 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { IonicModule } from "@ionic/angular";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { TvChannelsPageRoutingModule } from './tv-channels-routing.module';
-import { HeaderPageModule } from '../header/header.module';
-import { TvChannelsPage } from './tv-channels.page';
-import { ChannelsGridViewPageModule } from 'src/app/components/channels-grid-view/channels-grid-view.module';
+import { TvChannelsPageRoutingModule } from "./tv-channels-routing.module";
+import { HeaderPageModule } from "../header/header.module";
+import { TvChannelsPage } from "./tv-channels.page";
+import { ChannelsGridViewPageModule } from "src/app/components/channels-grid-view/channels-grid-view.module";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     TvChannelsPageRoutingModule,
     HeaderPageModule,
     ChannelsGridViewPageModule,
